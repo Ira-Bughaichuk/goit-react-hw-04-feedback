7feefdacc7d12411d439d7f0b55f5dbda2282ef0
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-02-10 19:57:47 +0200

    add file

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import s from './FeedbackOptions.module.scss';
+
+function FeedbackOptions({ options, onLeaveFeedback }) {
+
+    const element = options.map((option) => (
+        <li key={option} className={s.list}>
+            <button className={s.button} name={option} type="button" onClick={onLeaveFeedback}>{option}</button>
+        </li>
+    ));
+
+    return (
+       <ul> {element}</ul>
+    )
+}
+export default FeedbackOptions;
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string),
+}
